8e5262bad9099039897c345cc7468d7e379b78a9
Ilya Efarov <efarov.ily@mail.ru>
2026-08-10 17:33:50 +0700

    build: add Spring Boot 4 and Testcontainers

--- src/main/java/pro/itfray/App.java
+++
@@ -1,8 +0,0 @@
-package pro.itfray;
-
-public class App {
-
-  public static void main(String[] args) {
-    System.out.println("The swimming pool booking service is completed!!!");
-  }
-}
---
+++ src/main/java/pro/itfray/PoolBookingApplication.java
@@ -0,0 +1,12 @@
+package pro.itfray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolBookingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PoolBookingApplication.class, args);
+  }
+}
